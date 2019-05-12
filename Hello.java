e04b6a3e2b97e2d991223f2a316dbaa7cad7caea
Ramesh Chandra Sharma <rschandra27@gmail.com>
2019-05-12 09:08:31 +0545

    Added single line, Multiline and JavaDoc.

--- Hello.java
+++ Hello.java
@@ -1,5 +1,21 @@
 public class Hello
 {
+	//single line comment
+	
+	/*
+		Main Function:
+		This is a main function.
+		This function is a entry point of a program.
+
+	*/
+	/**
+		This Hello world program is the first program
+		which is used for teaching 5th semester 		student.
+		@author:Vikram
+		@since: 2019
+
+	**/
+
 	public static void main(String[] args)
 	{
 		System.out.println("Hello Java From BICTE");
